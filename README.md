b915635adce383e947f91e2804615bf0773c0bff
Cyrus Sahba <27029457+csahba@users.noreply.github.com>
2022-09-02 17:07:53 +0100

    method for crawling for category and product urls complete

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # webscraper-popsockets
 Scrapes popsocket website for product data using selenium
+
+
+to get category data have to scrape each product category page, at first tried to make a dictionary of sets containing category types for each product with the key being the url for the product, tried to use a recursive version of this for category pages, possibly with the idea of building a category tree datatype, later decided this was unwieldy for initial crawl and chose to just use a list of urls that I would split the strings of to extract category data.
--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
-from tabnanny import check
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
@@ -9,8 +9,12 @@ from time import sleep
 class Scraper():
 
     def __init__(self):
-        self.driver = webdriver.Chrome()
-        self.driver.set_window_size(1920, 1080)
+        chrome_options = webdriver.ChromeOptions()
+        chrome_options.add_argument('--blink-settings=imagesEnabled=false')
+        chrome_options.add_argument("--window-size=1920,1080")
+        self.driver = webdriver.Chrome(options=chrome_options)
+        self.category_list = []
+        self.product_dict = {}
 
     def get(self, url):
         self.driver.get(url)
@@ -41,9 +45,17 @@ class Scraper():
         first pass/crawl to retrieve a list of products by crawling through product list pages by category
         this category data is unretrieveable from the individual product pages so much be compiled from 'collection' pages
         '''
+        self.product_links = {}
         for url in self.category_list:
+
             self.get(url)
             self.show_more()
+            products = self.driver.find_elements(By.XPATH, '//div[contains(@class,"product-grid")]/div/div[contains(@class,"product")]')
+            url_list = [x.find_element(By.XPATH, './div/a').get_attribute('href').split('?')[0] for x in products]
+            self.product_links[url] = url_list
+
+        
+        return
 
     def get_category_links(self):
         '''
@@ -51,7 +63,7 @@ class Scraper():
         '''
         navbar = self.wait_for_element((By.XPATH,'//ul[contains(@class,"navbar-nav")]'),10)
         categories = navbar.find_elements(By.XPATH, './li[contains(@class, "nav-item-cat")]/a')
-        unchecked_category_urls = [x.get_attribute('href') for x in categories]
+        unchecked_category_urls = [x.get_attribute('href').split('?')[0] for x in categories]
         category_urls = []
         while True:
             if not unchecked_category_urls:
@@ -63,7 +75,7 @@ class Scraper():
             try:
                 self.get(url)
                 refinement_slider = self.wait_for_element((By.XPATH,'//div[@class="refinement-container"]'),10)
-                refinement_category_urls = [x.get_attribute('href') for x in refinement_slider.find_elements(By.XPATH, './/div[contains(@class,"tns-inner")]//a')]
+                refinement_category_urls = [x.get_attribute('href').split('?')[0] for x in refinement_slider.find_elements(By.XPATH, './/div[contains(@class,"tns-inner")]//a')]
                 unchecked_category_urls.extend(refinement_category_urls)
             except Exception:
                 pass
@@ -73,7 +85,7 @@ class Scraper():
 
     def show_more(self):
         '''
-        general method to scroll down and populate more products on catalogue/coselfllection page, should fail gracefully if page is fully populated
+        general method to populate more products on catalogue/collection page, should fail gracefully if page is fully populated
         '''
         while True:
             try:
@@ -97,7 +109,7 @@ if __name__ == '__main__':
     pop_scraper.accept_cookies()
     pop_scraper.decline_voucher()
     pop_scraper.get_category_links()
+    #pop_scraper.category_list = [ 'https://www.popsockets.co.uk/en-gb/popculture', 'https://www.popsockets.co.uk/en-gb/popculture/friends']  #['https://popsockets-promo.eu/', 'https://www.popsockets.co.uk/en-gb/popculture', 'https://www.popsockets.co.uk/en-gb/popculture/friends', 'https://www.popsockets.co.uk/en-gb/popculture/dc-comics', 'https://www.popsockets.co.uk/en-gb/popculture/star-wars', 'https://www.popsockets.co.uk/en-gb/popculture/star-wars/the-mandalorian', 'https://www.popsockets.co.uk/en-gb/popculture/star-wars/obi-wan-kenobi', 'https://www.popsockets.co.uk/en-gb/popculture/marvel', 'https://www.popsockets.co.uk/en-gb/popculture/disney', 'https://www.popsockets.co.uk/en-gb/popculture/disney/pixar', 'https://www.popsockets.co.uk/en-gb/popculture/disney/the-nightmare-before-christmas', 'https://www.popsockets.co.uk/en-gb/popculture/disney/mickey-and-minnie', 'https://www.popsockets.co.uk/en-gb/popculture/disney/lilo-and-stitch', 'https://www.popsockets.co.uk/en-gb/popculture/harry-potter', 'https://www.popsockets.co.uk/en-gb/popculture/all-popculture', 'https://www.popsockets.co.uk/en-gb/new']
+    
     pop_scraper.get_product_links()
-    #pop_scraper.get('https://www.popsockets.co.uk/en-gb/grips/premium')
-    #pop_scraper.show_more()
 
